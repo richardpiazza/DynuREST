15d801b67077d6f1654169caa02c302b3d938ae3
Richard Piazza <richard@richardpiazza.com>
2024-01-07 10:32:40 -0600

    Trusted Global Unicast Address (#5)
    Lookup local Trusted GUA (Global Unicast Address)

--- Sources/cli/IPCommand.swift
+++ Sources/cli/IPCommand.swift
@@ -18,20 +18,30 @@ struct IPCommand: AsyncParsableCommand {
     }
     
     enum Source: String, ExpressibleByArgument {
-        case ipify
-        case ifconfig
+        case ipifyApi
+        case ifconfigApi
+        case ifconfigCommand
+        
+        @available(*, deprecated, renamed: "ipifyApi")
+        static var ipify: Self { ipifyApi }
+        
+        @available(*, deprecated, renamed: "ifconfigApi")
+        static var ifconfig: Self { ifconfigApi }
     }
     
-    @Argument(help: "Lookup Source ['ipify', 'ifconfig']")
+    @Argument(help: "Lookup Source ['ipify', 'ifconfigApi', 'ifconfigCommand']")
     var source: Source
     
     func run() async throws {
         switch source {
-        case .ipify:
+        case .ipifyApi:
             let address = try await IPIfyClient.shared.ipAddress()
             print(address.description)
-        case .ifconfig:
+        case .ifconfigApi:
             let address = try await IFConfigClient.shared.ipAddress()
+            print(address.description)
+        case .ifconfigCommand:
+            let address = try await IFConfigCommand.shared.ipAddress()
             print(address.description)
         }
     }
